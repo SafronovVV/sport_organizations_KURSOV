79efaf9368874de89a7aaf542b84356ce60c77fa
Safronov Vladislav <safronov.zapas@gmail.com>
2018-03-01 21:32:43 +0300

    refactor links bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def navigation_links
+    [['Виды спорта', sports_path], ['Спорткомплексы', sport_complexes_path],
+    ['Организации', sport_organizations_path], ['Клубы', clubs_path],
+    ['Соревнования', competitions_path], ['Участники', participants_path],
+    ['Заявки', requests_path], ['Результаты', results_path]]
+  end
 end
