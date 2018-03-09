6606d079cc4232009eb879d8389d9b98ac60852f
Safronov Vladislav <safronov.zapas@gmail.com>
2018-03-09 20:47:56 +0300

    Add submit button name option to result helper

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,6 +1,6 @@
 module ResultsHelper
   def form_params
-    params[:action] == 'new' ? {header: 'Создание результата', create_form: true}\
-    : {header: 'Редактирование результата'}
+    params[:action] == 'new' ? {header: 'Создание результата', create_form: true, submit: 'Создать'}\
+    : {header: 'Редактирование результата', submit: 'Обновить'}
   end
 end
